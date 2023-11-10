865a1bf4e85d518fd785bed44e94f3b75f6d0858
shohitomi <shohitomi25@gmail.com>
2023-11-10 12:34:32 +0900

    displayed mobile image

--- app/page.tsx
+++ app/page.tsx
@@ -41,6 +41,13 @@ export default function Page() {
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop and monile verisons"
           />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing monile verison"
+          />
         </div>
       </div>
     </main>
