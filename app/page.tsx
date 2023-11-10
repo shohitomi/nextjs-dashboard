dd5110d573cd1375d8efd587ae89f181d6ac0f29
shohitomi <shohitomi25@gmail.com>
2023-11-10 10:32:43 +0900

    added hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "@/app/ui/home.module.css";
 import { lusitana } from "@/app/ui/fonts";
+import Image from "next/image";
 
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 import Link from "next/link";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -32,6 +34,13 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and monile verisons"
+          />
         </div>
       </div>
     </main>
